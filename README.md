294423ac30f98fb77fd24be8261767821e6fe257
rizzotto <demis@exxoss.com>
2016-01-05 15:55:01 +0100

    First real package config

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# activemq-deb-packaging
+# activeMQ-deb-packaging
+===================
+
+Simple debian packaging for Apache ActiveMQ
+
+### Changelog
+
+
+# Pre-requisites - Install fpm
+
+```sh
+$ sudo apt-get update
+$ sudo apt-get install ruby-dev build-essential
+$ sudo gem install fpm
+```
+# Options
+
+You can change the ActiveMQ version in dist_activemq on new versions of both.
+
+# Usage
+
+```sh
+$ ./dist_activemq.sh
+```
+
+# Installation
+
+```sh
+$ dpkg -i activemq_5.13.0-1_all.deb
+```
+
+or if you have your own repo:
+
+```sh
+$ ~/gpg-agent-headless.sh
+$ reprepro -b /var/repositories/ includedeb trusty $@
+$ apt-get install activemq
+```
+Note: Installs and runs as user 'activemq'. Easy to change for your needs.
+
+## Post install
+
+```sh
+
+$ sudo update-rc.d activemq defaults 25
+
+ Adding system startup for /etc/init.d/activemq ...
+   /etc/rc0.d/K25activemq -> ../init.d/activemq
+   /etc/rc1.d/K25activemq -> ../init.d/activemq
+   /etc/rc6.d/K25activemq -> ../init.d/activemq
+   /etc/rc2.d/S25activemq -> ../init.d/activemq
+   /etc/rc3.d/S25activemq -> ../init.d/activemq
+   /etc/rc4.d/S25activemq -> ../init.d/activemq
+   /etc/rc5.d/S25activemq -> ../init.d/activemq
+```
+
+## Tested Platforms
+
+* Debian Wheezy
+
+---
+
+## Package info
+Debian pkg: `activemq_5.13.0-1_all.deb`
+Version :
+  - ActiveMQ 5.13.0
+
+Init scripts:
+  - /etc/init.d/activemq
+
+Configuration:
+  - /etc/activemq
+  - /etc/default/activemq
+
+Logs:
+  - /var/log/activemq/
+
+Binaries:
+  - /usr/local/activemq/bin/
+
+Data:
+  - /var/lib/activemq/
+
+Network ports:
+  -
+
+Users:
+  - activemq : activemq
+
+---
---
+++ dist_activemq.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+set -u
+name=activemq
+version=5.13.0
+package_version="-1"
+description="Apache ActiveMQ is a message broker built around Java Message Service (JMS) API : allow sending messages between two or more clients in a loosely coupled, reliable, and asynchronous way."
+url="http://activemq.apache.org/"
+arch="all"
+section="misc"
+license="Apache Software License 2.0"
+bin_package="apache-activemq-${version}-bin.tar.gz"
+bin_download_url="http://www.apache.org/dyn/closer.cgi?path=/activemq/${version}/${bin_package}"
+origdir="$(pwd)"
+
+#_ MAIN _#
+rm -rf ${name}*.deb
+if [[ ! -f "${bin_package}" ]]; then
+  wget -c ${bin_download_url}
+fi
+mkdir -p tmp && pushd tmp
+rm -rf activemq
+mkdir -p activemq
+cd activemq
+mkdir -p build/usr/local/activemq
+mkdir -p build/etc/default
+mkdir -p build/etc/init
+mkdir -p build/etc/init.d
+mkdir -p build/etc/activemq
+mkdir -p build/var/log/activemq
+
+
+cp ${origdir}/files/config/default/activemq.default build/etc/default/activemq
+cp ${origdir}/files/config/init/activemq.init.d build/etc/init.d/activemq
+
+# Updated to use the Binary package
+
+tar zxf ${origdir}/${bin_package}
+cd apache-activemq-${version}
+
+# Config files
+mv etc/* ../build/etc/activemq
+cp ${origdir}/files/config/etc/org.ops4j.pax.logging.cfg ../build/etc/activemq
+cp ${origdir}/files/config/etc/org.ops4j.pax.url.mvn.cfg ../build/etc/activemq
+mv * ../build/usr/local/activemq/
+rmdir ../build/usr/local/activemq/etc
+
+cd ../build
+pushd usr/local/activemq
+popd
+
+fpm -t deb \
+    -n ${name} \
+    -v ${version}${package_version} \
+    --description "${description}" \
+    --url="{$url}" \
+    -a ${arch} \
+    --category ${section} \
+    --vendor "" \
+    --license "${license}" \
+    --config-files etc/activemq \
+    -m "${USER}@localhost" \
+    --prefix=/ \
+    -d oracle-j2sdk1.8 \
+    --after-install ${origdir}/files/build/postinst \
+    --after-remove ${origdir}/files/build/postrm \
+    -s dir \
+    -- .
+mv activemq*.deb ${origdir}
+popd
